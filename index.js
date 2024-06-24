b52888685d4fc4b70a084cc368911ba2e4eba872
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-06-24 07:11:11 +0600

    created get req for email

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ async function run() {
       res.send(user);
     });
 
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await users.findOne(query);
+      res.send(user);
+    });
+
     app.put("/users/update/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
